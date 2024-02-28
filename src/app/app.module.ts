6e353847dd536695f6ae8ed4d4928f7bad54e572
wzatta <wzjssa@gmail.com>
2024-02-28 20:11:58 -0300

    cap. 02 Criando componentes reutilizaveis - Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,24 @@ import { BannerComponent } from './shared/banner/banner.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { ContainerComponent } from './shared/container/container.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { CardComponent } from './shared/card/card.component';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
+import { MatCardModule } from '@angular/material/card';
+import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BannerComponent
+    BannerComponent,
+    ContainerComponent,
+    HomeComponent,
+    CardComponent,
+    CardBuscaComponent,
+    CardDepoimentoComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +35,8 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
 
   ],
   providers: [],
